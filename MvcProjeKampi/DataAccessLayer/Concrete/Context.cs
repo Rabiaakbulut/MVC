28554a42966b5a8dcfb59048ecbea86137cc4846
Rabia <61613991+Rabiaakbulut@users.noreply.github.com>
2022-02-09 20:34:25 +0300

    proje temeli

---
+++ MvcProjeKampi/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext //Context sınıfı DbContext sınıfından miras alsın
+    {
+        //context sınıfı tanımlanan propertyleri SQL'e tablo olarak yansıtacak
+
+        //pluralize isimlendirme
+        //about  -> projenin içinde yer alan sınıfın ismi
+        //abouts -> sql'e yansıyan tablonun ismi
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
